eec59dc8ee6e032c44bd8d2eb159f6625227195e
wf102 <william.ferguson83@gmail.com>
2026-08-28 21:52:37 +0100

    Refactor date handling

--- elbow.py
+++ elbow.py
@@ -21,6 +21,8 @@ def read_data(filename):
     df["flex_ma"] = df["flex"].rolling(window=WINDOW, center=True, min_periods=min_periods).mean()
     df["ext_ma"] = df["ext"].rolling(window=WINDOW, center=True, min_periods=min_periods).mean()
     df["range_ma"] = df["range"].rolling(window=WINDOW, center=True, min_periods=min_periods).mean()
+    df["date"] = [first_recorded_date + datetime.timedelta(days=i) for i in range(len(df))]
+
     return df
 
 def _mdate2days(x):
@@ -46,34 +48,34 @@ def _days2mdate(x):
 
 def plot_elbow(df):
 
-    dates = [first_recorded_date + datetime.timedelta(days=i) for i in range(len(df))]
+    last_date = df["date"].iloc[-1].to_pydatetime() + datetime.timedelta(days=14)
 
     fig, ax = plt.subplots(figsize=(14,8))
 
     plt.ylim(0,150)
-    plt.xlim(accident_date, dates[-1]+datetime.timedelta(days=14))
+    plt.xlim(accident_date, last_date)
     ax.set_xlabel("Date")
     ax.set_ylabel("Range [degrees]")
     ax.xaxis.set_major_locator(mdates.MonthLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m"))
 
-    max_days = (dates[-1] - orif_date).days
+    max_days = (last_date - orif_date).days
     secax = ax.secondary_xaxis('top', functions=(_mdate2days, _days2mdate))
     secax.set_xticks(range(0, max_days + 1, 20))
 
     ax.set_yticks(range(0, 151, 10))
     ax.grid(axis="y", linestyle="-", alpha=0.7)
 
-    plt.scatter(dates, df['ext'], color='red', marker='.', s=6)
-    plt.scatter(dates, df['flex'], color='red', marker='.', s=6)
-    plt.plot(dates, df['range_ma'], color='blue')
+    plt.scatter(df["date"], df['ext'], color='red', marker='.', s=6)
+    plt.scatter(df["date"], df['flex'], color='red', marker='.', s=6)
+    plt.plot(df["date"], df['range_ma'], color='blue')
 
     plt.axvline(x = orif_date, color = 'darkgrey', linestyle='--')
     plt.axvline(x = arthrolysis_date, color = 'darkgrey', linestyle='--')
     ax.text(orif_date+datetime.timedelta(days=-4), 12, 'ORIF', color='darkgrey', rotation=90)
     ax.text(arthrolysis_date+datetime.timedelta(days=-4), 12, 'Arthrolysis', color='darkgrey', rotation=90)
 
-    ax.fill_between(dates, df['ext_ma'], df['flex_ma'], color="red", alpha=0.3)
+    ax.fill_between(df["date"], df['ext_ma'], df['flex_ma'], color="red", alpha=0.3)
     ax.tick_params(axis='x', rotation=0)
 
     plt.tight_layout()
